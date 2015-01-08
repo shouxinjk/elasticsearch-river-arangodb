15a16c49d1a8cf26fe80e97e76dbe59c92798275
Patrick Huber <stackmagic@gmail.com>
2015-01-08 19:32:58 +0100

    actually make it a test

--- src/test/java/org/elasticsearch/river/arangodb/ArangoExceptionTest.java
+++ src/test/java/org/elasticsearch/river/arangodb/ArangoExceptionTest.java
@@ -1,9 +1,12 @@
 package org.elasticsearch.river.arangodb;
 
+import org.testng.annotations.Test;
+
 /** test the ArangoException */
 public class ArangoExceptionTest {
 
 	/** this is just for unit test coverage */
+	@Test
 	public void testCoverage() {
 		new ArangoException();
 		new ArangoException("message");
