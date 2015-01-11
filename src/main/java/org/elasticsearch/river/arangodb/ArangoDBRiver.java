1ec9fb301fb18b094ea6b61c9afd61720d003124
Patrick Huber <stackmagic@gmail.com>
2015-01-12 00:24:05 +0100

    move Indexer into its own class

--- src/main/java/org/elasticsearch/river/arangodb/ArangoDBRiver.java
+++ src/main/java/org/elasticsearch/river/arangodb/ArangoDBRiver.java
@@ -1,10 +1,7 @@
 package org.elasticsearch.river.arangodb;
 
 import static java.util.Arrays.asList;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.elasticsearch.client.Requests.indexRequest;
 import static org.elasticsearch.common.collect.Maps.newHashMap;
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.river.arangodb.ArangoConstants.BULK_SIZE_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.BULK_TIMEOUT_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.COLLECTION_FIELD;
@@ -21,18 +18,13 @@ import static org.elasticsearch.river.arangodb.ArangoConstants.NAME_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.OPTIONS_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.PASSWORD_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.PORT_FIELD;
-import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_ENTRY_UNDEFINED;
-import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_FIELD_KEY;
-import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_FIELD_TICK;
 import static org.elasticsearch.river.arangodb.ArangoConstants.RIVER_TYPE;
 import static org.elasticsearch.river.arangodb.ArangoConstants.SCRIPT_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.SCRIPT_TYPE_FIELD;
-import static org.elasticsearch.river.arangodb.ArangoConstants.STREAM_FIELD_OPERATION;
 import static org.elasticsearch.river.arangodb.ArangoConstants.THROTTLE_SIZE_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.TYPE_FIELD;
 import static org.elasticsearch.river.arangodb.ArangoConstants.USER_FIELD;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -43,21 +35,12 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedTransferQueue;
 
 import org.elasticsearch.ExceptionsHelper;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.block.ClusterBlockException;
-import org.elasticsearch.common.StopWatch;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.logging.ESLogger;
-import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.river.AbstractRiverComponent;
@@ -93,9 +76,11 @@ public class ArangoDBRiver extends AbstractRiverComponent implements River {
 
 	private final ExecutableScript script;
 
-	private volatile List<Slurper> slurpers = new ArrayList<Slurper>();
-	private volatile List<Thread> slurperThreads = new ArrayList<Thread>();
-	private volatile Thread indexerThread;
+	private List<Slurper> slurpers = new ArrayList<Slurper>();
+	private List<Thread> slurperThreads = new ArrayList<Thread>();
+	private Indexer indexer;
+	private Thread indexerThread;
+
 	private volatile boolean active = true;
 
 	private final BlockingQueue<Map<String, Object>> stream;
@@ -267,7 +252,8 @@ public class ArangoDBRiver extends AbstractRiverComponent implements River {
 			thread.start();
 		}
 
-		indexerThread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "arangodb_river_indexer").newThread(new Indexer());
+		indexer = new Indexer(this, client, typeName, indexName, arangoCollection, bulkTimeout, bulkSize, script, dropCollection, riverIndexName, riverName, stream);
+		indexerThread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "arangodb_river_indexer").newThread(indexer);
 		indexerThread.start();
 		logger.info("starting arangodb indexer");
 	}
@@ -324,263 +310,5 @@ public class ArangoDBRiver extends AbstractRiverComponent implements River {
 		}
 
 		return lastTick;
-	}
-
-
-
-	private class Indexer implements Runnable {
-		private final ESLogger logger = ESLoggerFactory.getLogger(this.getClass().getName());
-		private int deletedDocuments = 0;
-		private int insertedDocuments = 0;
-		private int updatedDocuments = 0;
-		private StopWatch sw;
-
-		@Override
-		public void run() {
-			logger.info("=== river-arangodb indexer running ... ===");
-
-			while (active) {
-				sw = new StopWatch().start();
-
-				deletedDocuments = 0;
-				insertedDocuments = 0;
-				updatedDocuments = 0;
-
-				try {
-					String lastTick = null;
-					BulkRequestBuilder bulk = client.prepareBulk();
-
-					// 1. Attempt to fill as much of the bulk request as possible
-					Map<String, Object> data = stream.take();
-					lastTick = updateBulkRequest(bulk, data);
-
-					while ((data = stream.poll(bulkTimeout.millis(), MILLISECONDS)) != null) {
-						lastTick = updateBulkRequest(bulk, data);
-
-						if (bulk.numberOfActions() >= bulkSize) {
-							break;
-						}
-					}
-
-					// 2. Update the Tick
-					if (lastTick != null) {
-						updateLastTick(arangoCollection, lastTick, bulk);
-					}
-
-					// 3. Execute the bulk requests
-					try {
-						BulkResponse response = bulk.execute().actionGet();
-
-						if (response.hasFailures()) {
-							logger.warn("failed to execute" + response.buildFailureMessage());
-						}
-					} catch (Exception e) {
-						logger.warn("failed to execute bulk", e);
-					}
-
-				} catch (InterruptedException e) {
-					if (logger.isDebugEnabled()) {
-						logger.debug("river-arangodb indexer interrupted");
-					}
-
-					Thread.currentThread().interrupt();
-				}
-
-				logStatistics();
-			}
-		}
-
-		private String updateBulkRequest(final BulkRequestBuilder bulk, Map<String, Object> data) {
-			String replogTick = (String) data.get(REPLOG_FIELD_TICK);
-			OpType operation = (OpType) data.get(STREAM_FIELD_OPERATION);
-			String objectId = "";
-
-			if (data.get(REPLOG_FIELD_KEY) != null) {
-				objectId = (String) data.get(REPLOG_FIELD_KEY);
-			}
-
-			data.remove(REPLOG_FIELD_TICK);
-			data.remove(STREAM_FIELD_OPERATION);
-
-			if (logger.isDebugEnabled()) {
-				logger.debug("updateBulkRequest for id: [{}], operation: [{}]", objectId, operation);
-				logger.debug("data: [{}]", data);
-			}
-
-			Map<String, Object> ctx = null;
-
-			try {
-				ctx = XContentFactory.xContent(XContentType.JSON).createParser("{}").mapAndClose();
-			} catch (IOException e) {
-				logger.warn("failed to parse {}", e);
-			}
-
-			if (script != null) {
-				if (ctx != null) {
-					ctx.put("doc", data);
-					ctx.put("operation", operation);
-
-					if (!objectId.isEmpty()) {
-						ctx.put("id", objectId);
-					}
-
-					if (logger.isDebugEnabled()) {
-						logger.debug("Context before script executed: {}", ctx);
-					}
-
-					script.setNextVar("ctx", ctx);
-
-					try {
-						script.run();
-						ctx = (Map<String, Object>) script.unwrap(ctx);
-					} catch (Exception e) {
-						logger.warn("failed to script process {}, ignoring", e, ctx);
-					}
-
-					if (logger.isDebugEnabled()) {
-						logger.debug("Context after script executed: {}", ctx);
-					}
-
-					if (ctx.containsKey("ignore") && ctx.get("ignore").equals(Boolean.TRUE)) {
-						logger.debug("From script ignore document id: {}", objectId);
-						return replogTick;
-					}
-
-					if (ctx.containsKey("deleted") && ctx.get("deleted").equals(Boolean.TRUE)) {
-						ctx.put("operation", "DELETE");
-					}
-
-					if (ctx.containsKey("doc")) {
-						data = (Map<String, Object>) ctx.get("doc");
-						logger.debug("From script document: {}", data);
-					}
-
-					if (ctx.containsKey("operation")) {
-						operation = OpType.valueOf(ctx.get("operation").toString());
-						logger.debug("From script operation: {}", operation);
-					}
-				}
-			}
-
-			try {
-				String index = extractIndex(ctx);
-				String type = extractType(ctx);
-				String parent = extractParent(ctx);
-				String routing = extractRouting(ctx);
-
-				if (logger.isDebugEnabled()) {
-					logger.debug("Operation: {} - index: {} - type: {} - routing: {} - parent: {}",
-							operation, index, type, routing, parent);
-				}
-
-				if (operation == OpType.INSERT) {
-					if (logger.isDebugEnabled()) {
-						logger.debug("Insert operation - id: {}", operation, objectId);
-					}
-
-					bulk.add(indexRequest(index).type(type).id(objectId)
-							.source(build(data, objectId)).routing(routing)
-							.parent(parent));
-
-					insertedDocuments++;
-				}
-				else if (operation == OpType.UPDATE) {
-					if (logger.isDebugEnabled()) {
-						logger.debug("Update operation - id: {}", objectId);
-					}
-
-					bulk.add(new DeleteRequest(index, type, objectId).routing(routing).parent(parent));
-					bulk.add(indexRequest(index).type(type).id(objectId).source(build(data, objectId)).routing(routing).parent(parent));
-
-					updatedDocuments++;
-				}
-				else if (operation == OpType.DELETE) {
-					if (logger.isDebugEnabled()) {
-						logger.debug("Delete operation - id: {}, data [{}]", objectId, data);
-					}
-
-					if (REPLOG_ENTRY_UNDEFINED.equals(objectId) && data.get(NAME_FIELD).equals(arangoCollection)) {
-						if (dropCollection) {
-							logger.info("Drop collection request [{}], [{}]", index, type);
-
-							bulk.request().requests().clear();
-							client.admin().indices().prepareDeleteMapping(index).setType(type).execute().actionGet();
-
-							deletedDocuments = 0;
-							updatedDocuments = 0;
-							insertedDocuments = 0;
-
-							logger.info("Delete request for index / type [{}] [{}] successfully executed.", index, type);
-						} else {
-							logger.info("Ignore drop collection request [{}], [{}]. The option has been disabled.", index, type);
-						}
-					} else {
-						logger.info("Delete request [{}], [{}], [{}]", index, type, objectId);
-
-						bulk.add(new DeleteRequest(index, type, objectId).routing(routing).parent(parent));
-
-						deletedDocuments++;
-					}
-				}
-			} catch (IOException e) {
-				logger.warn("failed to parse {}", e, data);
-			}
-
-			return replogTick;
-		}
-
-		private void updateLastTick(final String namespace, final String tick, final BulkRequestBuilder bulk) {
-			try {
-				bulk.add(indexRequest(riverIndexName)
-					.type(riverName.getName())
-					.id(namespace)
-					.source(jsonBuilder().startObject().startObject(RIVER_TYPE)
-						.field(LAST_TICK_FIELD, tick)
-						.endObject().endObject()));
-			} catch (IOException e) {
-				logger.error("error updating last Tick for collection {}", namespace);
-			}
-		}
-
-		private XContentBuilder build(final Map<String, Object> data, final String objectId) throws IOException {
-			return XContentFactory.jsonBuilder().map(data);
-		}
-
-		private String extractParent(Map<String, Object> ctx) {
-			return (String) ctx.get("_parent");
-		}
-
-		private String extractRouting(Map<String, Object> ctx) {
-			return (String) ctx.get("_routing");
-		}
-
-		private String extractType(Map<String, Object> ctx) {
-			String type = (String) ctx.get("_type");
-
-			if (type == null) {
-				type = typeName;
-			}
-
-			return type;
-		}
-
-		private String extractIndex(Map<String, Object> ctx) {
-			String index = (String) ctx.get("_index");
-
-			if (index == null) {
-				index = indexName;
-			}
-
-			return index;
-		}
-
-		private void logStatistics() {
-			long totalDocuments = deletedDocuments + insertedDocuments;
-			long totalTimeInSeconds = sw.stop().totalTime().seconds();
-			long totalDocumentsPerSecond = (totalTimeInSeconds == 0) ? totalDocuments : totalDocuments / totalTimeInSeconds;
-
-			logger.info("Indexed {} documents, {} insertions {}, updates, {} deletions, {} documents per second",
-					totalDocuments, insertedDocuments, updatedDocuments, deletedDocuments, totalDocumentsPerSecond);
-		}
 	}
 }
---
+++ src/main/java/org/elasticsearch/river/arangodb/Indexer.java
@@ -0,0 +1,322 @@
+package org.elasticsearch.river.arangodb;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.elasticsearch.client.Requests.indexRequest;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.river.arangodb.ArangoConstants.LAST_TICK_FIELD;
+import static org.elasticsearch.river.arangodb.ArangoConstants.NAME_FIELD;
+import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_ENTRY_UNDEFINED;
+import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_FIELD_KEY;
+import static org.elasticsearch.river.arangodb.ArangoConstants.REPLOG_FIELD_TICK;
+import static org.elasticsearch.river.arangodb.ArangoConstants.RIVER_TYPE;
+import static org.elasticsearch.river.arangodb.ArangoConstants.STREAM_FIELD_OPERATION;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.StopWatch;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.river.RiverName;
+import org.elasticsearch.script.ExecutableScript;
+
+public class Indexer implements Runnable {
+
+	private final ESLogger logger = ESLoggerFactory.getLogger(this.getClass().getName());
+	private int deletedDocuments = 0;
+	private int insertedDocuments = 0;
+	private int updatedDocuments = 0;
+	private StopWatch sw;
+
+	private final ArangoDBRiver river;
+	private final Client client;
+	private final String typeName;
+	private final String indexName;
+	private final String arangoCollection;
+	private final TimeValue bulkTimeout;
+	private final int bulkSize;
+	private final ExecutableScript script;
+	private final boolean dropCollection;
+	private final String riverIndexName;
+	private final RiverName riverName;
+	private final BlockingQueue<Map<String, Object>> stream;
+
+	public Indexer( //
+	ArangoDBRiver river, //
+		Client client, //
+		String typeName, //
+		String indexName, //
+		String arangoCollection, //
+		TimeValue bulkTimeout, //
+		int bulkSize, //
+		ExecutableScript script, //
+		boolean dropCollection, //
+		String riverIndexName, //
+		RiverName riverName, //
+		BlockingQueue<Map<String, Object>> stream //
+	) {
+		this.river = river;
+		this.client = client;
+		this.typeName = typeName;
+		this.indexName = indexName;
+		this.arangoCollection = arangoCollection;
+		this.bulkTimeout = bulkTimeout;
+		this.bulkSize = bulkSize;
+		this.script = script;
+		this.dropCollection = dropCollection;
+		this.riverIndexName = riverIndexName;
+		this.riverName = riverName;
+		this.stream = stream;
+	}
+
+	@Override
+	public void run() {
+		logger.info("=== river-arangodb indexer running ... ===");
+
+		while (river.isActive()) {
+			sw = new StopWatch().start();
+
+			deletedDocuments = 0;
+			insertedDocuments = 0;
+			updatedDocuments = 0;
+
+			try {
+				String lastTick = null;
+				BulkRequestBuilder bulk = client.prepareBulk();
+
+				// 1. Attempt to fill as much of the bulk request as possible
+				Map<String, Object> data = stream.take();
+				lastTick = updateBulkRequest(bulk, data);
+
+				while ((data = stream.poll(bulkTimeout.millis(), MILLISECONDS)) != null) {
+					lastTick = updateBulkRequest(bulk, data);
+					if (bulk.numberOfActions() >= bulkSize) {
+						break;
+					}
+				}
+
+				// 2. Update the Tick
+				if (lastTick != null) {
+					updateLastTick(arangoCollection, lastTick, bulk);
+				}
+
+				// 3. Execute the bulk requests
+				try {
+					BulkResponse response = bulk.execute().actionGet();
+					if (response.hasFailures()) {
+						logger.warn("failed to execute" + response.buildFailureMessage());
+					}
+				}
+				catch (Exception e) {
+					logger.warn("failed to execute bulk", e);
+				}
+			}
+			catch (InterruptedException e) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("river-arangodb indexer interrupted");
+				}
+				Thread.currentThread().interrupt();
+			}
+			logStatistics();
+		}
+	}
+
+	private String updateBulkRequest(final BulkRequestBuilder bulk, Map<String, Object> data) {
+		String replogTick = (String) data.get(REPLOG_FIELD_TICK);
+		OpType operation = (OpType) data.get(STREAM_FIELD_OPERATION);
+		String objectId = "";
+
+		if (data.get(REPLOG_FIELD_KEY) != null) {
+			objectId = (String) data.get(REPLOG_FIELD_KEY);
+		}
+
+		data.remove(REPLOG_FIELD_TICK);
+		data.remove(STREAM_FIELD_OPERATION);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("updateBulkRequest for id: [{}], operation: [{}]", objectId, operation);
+			logger.debug("data: [{}]", data);
+		}
+
+		Map<String, Object> ctx = null;
+
+		try {
+			ctx = XContentFactory.xContent(XContentType.JSON).createParser("{}").mapAndClose();
+		}
+		catch (IOException e) {
+			logger.warn("failed to parse {}", e);
+		}
+
+		if (script != null) {
+			if (ctx != null) {
+				ctx.put("doc", data);
+				ctx.put("operation", operation);
+
+				if (!objectId.isEmpty()) {
+					ctx.put("id", objectId);
+				}
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("Context before script executed: {}", ctx);
+				}
+
+				script.setNextVar("ctx", ctx);
+
+				try {
+					script.run();
+					ctx = (Map<String, Object>) script.unwrap(ctx);
+				}
+				catch (Exception e) {
+					logger.warn("failed to script process {}, ignoring", e, ctx);
+				}
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("Context after script executed: {}", ctx);
+				}
+
+				if (ctx.containsKey("ignore") && ctx.get("ignore").equals(Boolean.TRUE)) {
+					logger.debug("From script ignore document id: {}", objectId);
+					return replogTick;
+				}
+
+				if (ctx.containsKey("deleted") && ctx.get("deleted").equals(Boolean.TRUE)) {
+					ctx.put("operation", "DELETE");
+				}
+
+				if (ctx.containsKey("doc")) {
+					data = (Map<String, Object>) ctx.get("doc");
+					logger.debug("From script document: {}", data);
+				}
+
+				if (ctx.containsKey("operation")) {
+					operation = OpType.valueOf(ctx.get("operation").toString());
+					logger.debug("From script operation: {}", operation);
+				}
+			}
+		}
+
+		try {
+			String index = extractIndex(ctx);
+			String type = extractType(ctx);
+			String parent = extractParent(ctx);
+			String routing = extractRouting(ctx);
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("Operation: {} - index: {} - type: {} - routing: {} - parent: {}", operation, index, type, routing, parent);
+			}
+
+			if (operation == OpType.INSERT) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("Insert operation - id: {}", operation, objectId);
+				}
+
+				bulk.add(indexRequest(index).type(type).id(objectId).source(build(data, objectId)).routing(routing).parent(parent));
+
+				insertedDocuments++;
+			}
+			else if (operation == OpType.UPDATE) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("Update operation - id: {}", objectId);
+				}
+
+				bulk.add(new DeleteRequest(index, type, objectId).routing(routing).parent(parent));
+				bulk.add(indexRequest(index).type(type).id(objectId).source(build(data, objectId)).routing(routing).parent(parent));
+
+				updatedDocuments++;
+			}
+			else if (operation == OpType.DELETE) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("Delete operation - id: {}, data [{}]", objectId, data);
+				}
+
+				if (REPLOG_ENTRY_UNDEFINED.equals(objectId) && data.get(NAME_FIELD).equals(arangoCollection)) {
+					if (dropCollection) {
+						logger.info("Drop collection request [{}], [{}]", index, type);
+
+						bulk.request().requests().clear();
+						client.admin().indices().prepareDeleteMapping(index).setType(type).execute().actionGet();
+
+						deletedDocuments = 0;
+						updatedDocuments = 0;
+						insertedDocuments = 0;
+
+						logger.info("Delete request for index / type [{}] [{}] successfully executed.", index, type);
+					}
+					else {
+						logger.info("Ignore drop collection request [{}], [{}]. The option has been disabled.", index, type);
+					}
+				}
+				else {
+					logger.info("Delete request [{}], [{}], [{}]", index, type, objectId);
+
+					bulk.add(new DeleteRequest(index, type, objectId).routing(routing).parent(parent));
+
+					deletedDocuments++;
+				}
+			}
+		}
+		catch (IOException e) {
+			logger.warn("failed to parse {}", e, data);
+		}
+
+		return replogTick;
+	}
+
+	private void updateLastTick(final String namespace, final String tick, final BulkRequestBuilder bulk) {
+		try {
+			bulk.add(indexRequest(riverIndexName).type(riverName.getName()).id(namespace).source(jsonBuilder().startObject().startObject(RIVER_TYPE).field(LAST_TICK_FIELD, tick).endObject().endObject()));
+		}
+		catch (IOException e) {
+			logger.error("error updating last Tick for collection {}", namespace);
+		}
+	}
+
+	private XContentBuilder build(final Map<String, Object> data, final String objectId) throws IOException {
+		return XContentFactory.jsonBuilder().map(data);
+	}
+
+	private String extractParent(Map<String, Object> ctx) {
+		return (String) ctx.get("_parent");
+	}
+
+	private String extractRouting(Map<String, Object> ctx) {
+		return (String) ctx.get("_routing");
+	}
+
+	private String extractType(Map<String, Object> ctx) {
+		String type = (String) ctx.get("_type");
+
+		if (type == null) {
+			type = typeName;
+		}
+
+		return type;
+	}
+
+	private String extractIndex(Map<String, Object> ctx) {
+		String index = (String) ctx.get("_index");
+
+		if (index == null) {
+			index = indexName;
+		}
+
+		return index;
+	}
+
+	private void logStatistics() {
+		long totalDocuments = deletedDocuments + insertedDocuments;
+		long totalTimeInSeconds = sw.stop().totalTime().seconds();
+		long totalDocumentsPerSecond = (totalTimeInSeconds == 0) ? totalDocuments : totalDocuments / totalTimeInSeconds;
+
+		logger.info("Indexed {} documents, {} insertions {}, updates, {} deletions, {} documents per second", totalDocuments, insertedDocuments, updatedDocuments, deletedDocuments, totalDocumentsPerSecond);
+	}
+}
