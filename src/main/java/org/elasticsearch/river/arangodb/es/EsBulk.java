335943b9bc560b917ed83d820d38ea68532c5198
Patrick Huber <stackmagic@gmail.com>
2015-02-02 15:27:21 +0100

    the converter isn't needed

--- src/main/java/org/elasticsearch/river/arangodb/es/EsBulk.java
+++ src/main/java/org/elasticsearch/river/arangodb/es/EsBulk.java
@@ -1,7 +1,5 @@
 package org.elasticsearch.river.arangodb.es;
 
-import net.swisstech.arangodb.model.wal.WalEvent;
-
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
@@ -18,26 +16,18 @@ public class EsBulk {
 
 	private final Client client;
 	private final Ticks ticks;
-	private final WalEventConverter converter;
 
 	private BulkRequestBuilder builder;
 
 	@Inject
-	public EsBulk(Client client, Ticks ticks, WalEventConverter converter) {
+	public EsBulk(Client client, Ticks ticks) {
 		this.client = client;
 		this.ticks = ticks;
-		this.converter = converter;
 	}
 
 	public void add(Tick tick) {
 		IndexRequest tickUpdate = ticks.buildSaveReq(tick);
 		builder.add(tickUpdate);
-	}
-
-	/** @returns the tick of the event processed */
-	public long add(WalEvent event) {
-		// TODO
-		return 0;
 	}
 
 	public int size() {
